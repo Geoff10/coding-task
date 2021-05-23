3046aaceb2a912a8271945087ea19adcfc06a0a1
Geoff <George.Jukes@googlemail.com>
2021-05-23 09:19:39 +0100

    Create vue components for viewing shopping lists - Item.vue displays the details of an indivdual item - List.vue displays a set of Item components

---
+++ resources/js/Components/ShoppingList/Item.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="py-2 border-t border-gray-300">
+        <div class="px-1">
+            {{ item.name }}
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: {
+            item: Object,
+        }
+    }
+</script>
---
+++ resources/js/Components/ShoppingList/List.vue
@@ -0,0 +1,33 @@
+<template>
+    <div v-if="hasItems">
+        <div class="py-2 font-bold">
+            <div class="px-1">Item</div>
+        </div>
+        <shopping-list-item v-for="(item, id) in items" :key="id" v-model:item="items[id]" :id="'shopping-item-' + id"></shopping-list-item>
+    </div>
+    <div v-else>No items on the shopping list yet.</div>
+</template>
+
+<script>
+    import ShoppingListItem from '@/Components/ShoppingList/Item'
+
+    export default {
+        components: {
+            ShoppingListItem,
+        },
+
+        props: {
+            items: {
+                type: Array,
+                default: null
+            }
+        },
+
+        computed: {
+            hasItems() {
+                return this.items && (this.items.length > 0);
+            }
+        },
+    }
+</script>
+
