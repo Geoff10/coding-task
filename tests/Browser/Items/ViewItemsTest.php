d7617baf63fe28df4e76890978fcab0a5a6ddc71
Geoff <George.Jukes@googlemail.com>
2021-05-23 13:05:09 +0100

    Add browser test to check only user items are listed

--- tests/Browser/Items/ViewItemsTest.php
+++ tests/Browser/Items/ViewItemsTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Browser\ShoppingList;
 
 use App\Models\Item;
+use App\Models\User;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Laravel\Dusk\Browser;
 use Tests\DuskTestCase;
@@ -25,6 +26,30 @@ class ViewItemsTest extends DuskTestCase
 
             foreach ($items as $item) {
                 $browser->assertSee($item->name);
+            }
+        });
+    }
+
+    /**
+     * Check that only a user's items are visible.
+     *
+     * @return void
+     */
+    public function testOnlyUsersItemListCanBeViewed()
+    {
+        $user = User::factory()->create();
+        $user_items = Item::factory()->count(2)->for($user)->create();
+        $other_items = Item::factory()->count(2)->create();
+
+        $this->browse(function (Browser $browser) use ($user_items, $other_items) {
+            $browser->visit('/items');
+
+            foreach ($user_items as $item) {
+                $browser->assertSee($item->name);
+            }
+
+            foreach ($other_items as $item) {
+                $browser->assertDontSee($item->name);
             }
         });
     }
