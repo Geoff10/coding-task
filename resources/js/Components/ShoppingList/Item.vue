a8223cafc8a238c1f58fa2e10f9852fc70deef13
Geoff <George.Jukes@googlemail.com>
2021-05-23 11:00:36 +0100

    Add checkbox to allow crossing off items

--- resources/js/Components/ShoppingList/Item.vue
+++ resources/js/Components/ShoppingList/Item.vue
@@ -1,14 +1,21 @@
 <template>
-    <div class="py-2 border-t border-gray-300 shopping-item">
-        <div class="px-1">
-            <span class="name">{{ item.name }}</span>
+    <div class="py-2 border-t border-gray-300 flex items-center shopping-item">
+        <checkbox v-model="item.purchased"></checkbox>
+        <div class="px-2 flex-grow">
+            <span class="name" :class="{'line-through': item.purchased}">{{ item.name }}</span>
             <a class="text-red-600 delete-btn inline-block ml-4 hover:underline hover:cursor-pointer" @click="deleteItem()">Delete</a>
         </div>
     </div>
 </template>
 
 <script>
+    import Checkbox from '@/Components/Checkbox';
+
     export default {
+        components: {
+            Checkbox,
+        },
+
         props: {
             item: Object,
         },
