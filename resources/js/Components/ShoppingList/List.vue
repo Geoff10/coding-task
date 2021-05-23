ead042815cd9e1ab1b8f84fd2268977a3f5a9b3a
Geoff <George.Jukes@googlemail.com>
2021-05-23 11:58:52 +0100

    Fix adding items when list is empty

--- resources/js/Components/ShoppingList/List.vue
+++ resources/js/Components/ShoppingList/List.vue
@@ -1,12 +1,12 @@
 <template>
-    <div v-if="hasItems">
-        <div class="py-2 font-bold">
-            <div class="px-1">Item</div>
-        </div>
-        <add-shopping-list-item @addItem="$emit('addItem', $event)"></add-shopping-list-item>
-        <shopping-list-item v-for="(item, id) in items" :key="id" v-model:item="items[id]" :id="'shopping-item-' + id" @deleteItem="$emit('deleteItem', id)"></shopping-list-item>
+    <div class="py-2 font-bold">
+        <div class="px-1">Item</div>
     </div>
-    <div v-else>No items on the shopping list yet.</div>
+    <add-shopping-list-item @addItem="$emit('addItem', $event)"></add-shopping-list-item>
+    <div v-if="hasItems">
+        <shopping-list-item v-for="(item, id) in items" :key="id" v-model:item="items[id]" :id="'shopping-item-' + item.id" @deleteItem="$emit('deleteItem', item.id)"></shopping-list-item>
+    </div>
+    <div v-else class="px-1 py-2">No items on the shopping list yet.</div>
 </template>
 
 <script>
