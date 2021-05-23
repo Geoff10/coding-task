e87775729c25c50e12c4c3abc45cb53882a64620
Geoff <George.Jukes@googlemail.com>
2021-05-23 11:34:35 +0100

    Create factory for Item model

---
+++ database/factories/ItemFactory.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Item;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class ItemFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Item::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->name(),
+            'purchased' => false,
+        ];
+    }
+
+    /**
+     * Indicate that the item is purchased.
+     *
+     * @return \Illuminate\Database\Eloquent\Factories\Factory
+     */
+    public function purchased()
+    {
+        return $this->state(function (array $attributes) {
+            return [
+                'purchased' => true,
+            ];
+        });
+    }
+}
