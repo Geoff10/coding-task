ec4b8dad1a715a6efde4ee65fdcfd888ef7aa600
Geoff <George.Jukes@googlemail.com>
2021-05-23 09:21:50 +0100

    Create a webpage to display the shopping list - Adds a route entry - Create the ItemController for loading the index page - Create a vue.js Index page and layout file

---
+++ resources/js/Layouts/Unauthenticated.vue
@@ -0,0 +1,83 @@
+<template>
+    <div>
+        <div class="min-h-screen bg-gray-100">
+            <nav class="bg-white border-b border-gray-100">
+                <!-- Primary Navigation Menu -->
+                <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+                    <div class="flex justify-between h-16">
+                        <div class="flex">
+                            <!-- Navigation Links -->
+                            <div class="hidden space-x-8 sm:-my-px sm:flex">
+                                <breeze-nav-link :href="route('items.index')" :active="route().current('items.index')">
+                                    Shopping List
+                                </breeze-nav-link>
+                            </div>
+                        </div>
+
+                        <div class="hidden sm:flex sm:items-center sm:ml-6">
+                        </div>
+
+                        <!-- Hamburger -->
+                        <div class="-mr-2 flex items-center sm:hidden">
+                            <button @click="showingNavigationDropdown = ! showingNavigationDropdown" class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out">
+                                <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
+                                    <path :class="{'hidden': showingNavigationDropdown, 'inline-flex': ! showingNavigationDropdown }" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
+                                    <path :class="{'hidden': ! showingNavigationDropdown, 'inline-flex': showingNavigationDropdown }" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
+                                </svg>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+
+                <!-- Responsive Navigation Menu -->
+                <div :class="{'block': showingNavigationDropdown, 'hidden': ! showingNavigationDropdown}" class="sm:hidden">
+                    <div class="pt-2 pb-3 space-y-1">
+                        <breeze-responsive-nav-link :href="route('dashboard')" :active="route().current('dashboard')">
+                            Dashboard
+                        </breeze-responsive-nav-link>
+                    </div>
+
+                    <!-- Responsive Settings Options -->
+                    <div class="pt-4 pb-1 border-t border-gray-200">
+                    </div>
+                </div>
+            </nav>
+
+            <!-- Page Heading -->
+            <header class="bg-white shadow" v-if="$slots.header">
+                <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+                    <slot name="header" />
+                </div>
+            </header>
+
+            <!-- Page Content -->
+            <main>
+                <slot />
+            </main>
+        </div>
+    </div>
+</template>
+
+<script>
+    import BreezeApplicationLogo from '@/Components/ApplicationLogo'
+    import BreezeDropdown from '@/Components/Dropdown'
+    import BreezeDropdownLink from '@/Components/DropdownLink'
+    import BreezeNavLink from '@/Components/NavLink'
+    import BreezeResponsiveNavLink from '@/Components/ResponsiveNavLink'
+
+    export default {
+        components: {
+            BreezeApplicationLogo,
+            BreezeDropdown,
+            BreezeDropdownLink,
+            BreezeNavLink,
+            BreezeResponsiveNavLink,
+        },
+
+        data() {
+            return {
+                showingNavigationDropdown: false,
+            }
+        },
+    }
+</script>
---
+++ resources/js/Pages/Items/Index.vue
@@ -0,0 +1,37 @@
+<template>
+    <breeze-unauthenticated-layout>
+        <template #header>
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                Shopping List
+            </h2>
+        </template>
+
+        <div class="py-12">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                    <div class="p-6 bg-white border-b border-gray-200">
+                        <shopping-list :items="items"></shopping-list>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </breeze-unauthenticated-layout>
+</template>
+
+<script>
+    import BreezeUnauthenticatedLayout from '@/Layouts/Unauthenticated'
+    import ShoppingList from '@/Components/ShoppingList/List'
+
+    export default {
+        components: {
+            BreezeUnauthenticatedLayout,
+            ShoppingList,
+        },
+
+        props: {
+            auth: Object,
+            errors: Object,
+            items: Array,
+        },
+    }
+</script>
